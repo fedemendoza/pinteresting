df13078d47bd7851bfd5bebd9de96f83694c7f76
Federico Mendoza <me@fedemendoza.com>
2014-03-12 03:30:59 -0600

    Add Amazon S3 for Paperclip uploads to Heroku for international users

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,3 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':omrpinterest.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
